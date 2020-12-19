d184b721716d8aa6ae5d017ec154b2b8aae5130c
keshar <paudelkeshar2055@gmail.com>
2020-12-19 16:23:28 +0545

    Authorization server with mysql databse

---
+++ src/main/java/com/keshar/rentcloud/autorizationserver/repository/UserDetailRepository.java
@@ -0,0 +1,12 @@
+package com.keshar.rentcloud.autorizationserver.repository;
+
+import com.keshar.rentcloud.autorizationserver.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserDetailRepository extends JpaRepository<User,Integer> {
+
+    Optional<User> findByUsername(String username);
+
+}
---
+++ src/test/java/com/keshar/rentcloud/autorizationserver/AutorizationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keshar.rentcloud.autorizationserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutorizationServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
